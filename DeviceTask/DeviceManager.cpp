28a529ef4a164b9e0d8facfff472f7827466cbf9
maxrangner <max@rangner.com>
2025-10-20 17:06:45 +0200

    Add feature: Remove Device.

--- DeviceTask/DeviceManager.cpp
+++ DeviceTask/DeviceManager.cpp
@@ -16,6 +16,13 @@ public:
     void addDevice(Device d) {
         devices.push_back(d);
     }
+    void removeDevice(int id) {
+        for (int i = 0; i < devices.size(); i++) {
+            if (devices[i].id == id) {
+                devices.erase(devices.begin()+i);
+            }
+        }
+    }
     void printAll() const {
         for (const auto& d : devices) {
             cout << d.id << " - " << d.name << " (" << d.value << ")" << endl;
@@ -28,4 +35,5 @@ int main() {
     Device d1 = { 1, "TempSensor", 21.4 };
     manager.addDevice(d1);
     manager.printAll();
+    // manager.removeDevice(1);
 }
