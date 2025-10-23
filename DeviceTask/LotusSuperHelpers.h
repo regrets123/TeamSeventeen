e68f6b1adb7261df74672507e181b1cc291efe2b
Lotus <lotus.saxe@gmail.com>
2025-10-23 13:29:38 +0200

    Best code ever.

---
+++ DeviceTask/LotusSuperHelpers.h
@@ -0,0 +1,14 @@
+//
+// Created by hula_ on 2025-10-23.
+//
+#pragma once
+#include <string>
+
+
+
+class LotusSuperHelpers {
+public:
+    static void WillMakeYourLifeAmazing();
+    static std::string FixesAllStringIssues(std::string toFix);
+    static float BestComparerEver(float a, float b);
+};
